965af5c4e9300e84eab5506802b571679f4a8d17
Nisha <nishahs08@outlook.in>
2017-07-10 23:17:35 +0530

    Added database config

--- config/index.js
+++ config/index.js
@@ -3,8 +3,11 @@
 // Attach env file options on to process.env
 require('dotenv').config();
 
+const dbConfig = require('./db');
+
 // Exporting a function that will be called by bootstrap/app.js to configure app.
 // This function accepts a reference to express app.
 module.exports = function (app) {
 
+	dbConfig();
 }
