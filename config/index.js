37c4ccb84efe8fd0c3944808625ae0c20419c1ca
Nisha <nishahs08@outlook.in>
2017-07-10 23:03:46 +0530

    Added configuration for app

---
+++ config/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Exporting a function that will be called by bootstrap/app.js to configure app.
+// This function accepts a reference to express app.
+module.exports = function (app) {
+
+}
