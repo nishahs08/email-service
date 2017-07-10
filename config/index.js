f58a2a8634c99814b551b4b2355d4ee52383a6e0
Nisha <nishahs08@outlook.in>
2017-07-10 23:08:21 +0530

    added process env options

--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Attach env file options on to process.env
+require('dotenv').config();
+
 // Exporting a function that will be called by bootstrap/app.js to configure app.
 // This function accepts a reference to express app.
 module.exports = function (app) {
