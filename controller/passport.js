c731dd8cf32743b2f9a23e29c0768a010e6f9871
Himanshu Singh <himanshuakatalli@live.com>
2017-07-10 19:58:28 +0530

    Fixed passport

--- controller/passport.js
+++ controller/passport.js
@@ -2,15 +2,30 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 const User = require('./../models/user');
 
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
 
-passport.use(new LocalStrategy(
+/*
+* Attaching a function to deserialize the user from the passport session.
+* DeSerializing the process of fetching the user data from the only param stored in passport session.
+*/
+passport.deserializeUser(function(id, done) {
+  User.findOne({ username: id })
+    .then(user => done(user))
+    .catch(err => done(null, false))
+});
+
+// Have renamed the localstrategy to be used by name local
+passport.use('local', new LocalStrategy(
   function(username, password, done) {
+    console.log('ASDAS');
     User.findOne({ username: username }, function(err, user) {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
+      if (user.password !== password) {
         return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var router = express.Router()
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var userController = require('./../controller/user');
+
 router.post('/register', userController.register);
 router.get('/usersList',userController.listOfUsers);
-router.post('/login', passport.authenticate('LocalStrategy'), function(req, res) {
-      res.redirect('/');
-  });
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.json({
+      a: true
+    });
+});
 
 
 module.exports = router;
