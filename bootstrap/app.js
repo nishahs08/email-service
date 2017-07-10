55cd07a81bbe22d61ee5ac86757be06409543b2d
Nisha Singh <nishahs08@outlook.in>
2017-07-10 19:25:58 +0530

    passport

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const parser =require('body-parser');
 const userRoute = require('./../controller/user');
+const route = require('./../routes/user');
 const app = express();
+const passport = require('passport')
+const User = require('./../models/user');
+
+var LocalStrategy = require('passport-local').Strategy;
 
 app.use(parser.json({ }));
 app.use(parser.urlencoded({ extended: false }));
-// app.use('/api/user',userRoute.register)
-app.post('/api/user',userRoute.register)
+passport.authenticate();
+app.use(route);
+//app.post('/api/user',userRoute.register);
+//app.get('/usersList',userRoute.listOfUsers);
+
 module.exports = app;
 
